5751f909d67a24746c2589c62da2f46de52311bd
lovrodukic <ldukic@calpoly.edu>
2021-12-07 14:33:50 -0800

    Cactus

---
+++ src/Cactus.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.util.function.Function;
+
+import processing.core.PImage;
+public class Cactus extends Entity{
+
+
+
+    public Cactus(
+            String id,
+            Point position,
+            List<PImage> images)
+
+    {
+        super(id,position,images);
+    }
+
+
+
+}
+
